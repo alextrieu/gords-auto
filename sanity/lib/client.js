a7bd5889cf1f650e67356eb2bb542c386d167ac8
Alex <trieu.alex1@gmail.com>
2024-11-09 22:43:39 -0800

    update project id

--- sanity/lib/client.js
+++ sanity/lib/client.js
@@ -1,10 +1,10 @@
-import { createClient } from 'next-sanity'
+import { createClient } from "next-sanity";
 
-import { apiVersion, dataset, projectId } from '../env'
+import { apiVersion, dataset } from "../env";
 
 export const client = createClient({
-  projectId,
+  projectId: "t1hlsh4j",
   dataset,
   apiVersion,
   useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
-})
+});
