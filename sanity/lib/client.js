1a392179516ffb83316918be5bfb949e132073dc
Alex <trieu.alex1@gmail.com>
2024-11-09 23:00:44 -0800

    hardcode client values

--- sanity/lib/client.js
+++ sanity/lib/client.js
@@ -3,8 +3,8 @@ import { createClient } from "next-sanity";
 // import { apiVersion, dataset } from "../env";
 
 export const client = createClient({
-  projectId: process.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  dataset: process.NEXT_PUBLIC_SANITY_DATASET,
+  projectId: "t1hlsh4j",
+  dataset: "production",
   apiVersion: "2024-11-10",
   useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
 });
