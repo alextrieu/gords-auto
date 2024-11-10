8cc306db48fed3b00402f5d15469badde76a4011
Alex <trieu.alex1@gmail.com>
2024-11-09 22:55:40 -0800

    update client.js to read env

--- sanity/lib/client.js
+++ sanity/lib/client.js
@@ -1,10 +1,10 @@
 import { createClient } from "next-sanity";
 
-import { apiVersion, dataset } from "../env";
+// import { apiVersion, dataset } from "../env";
 
 export const client = createClient({
-  projectId: NEXT_PUBLIC_SANITY_PROJECT_ID,
-  dataset: NEXT_PUBLIC_SANITY_DATASET,
+  projectId: process.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  dataset: process.NEXT_PUBLIC_SANITY_DATASET,
   apiVersion: "2024-11-10",
   useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
 });
